8a592662d6deee87e2a65e24d2b48ffe20152b8b
0verEngineer <dev@hackinger.io>
2023-07-02 01:00:23 +0200

    Add libssh2 import and versino

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 
 #include <iostream>
 #include <zlib.h>
+#include <libssh2_sftp.h>
 
 int main(int argc, char *argv[])
 {
+    printf("Libssh2 version: %s\n", libssh2_version(0));
     printf("ZLIB VERSION: %s\n", zlibVersion());
     std::cout << "Hello World!" << std::endl;
 }
